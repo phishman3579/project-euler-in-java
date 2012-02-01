e141e2f1a4bd3d26fbbf1e31b2bb896ea69ebaaa
phishman3579 <phishman3579@2f9ebcba-fdc9-2f80-e423-1e4cc5a588e7>
2012-02-01 16:47:16 +0000

    Added ideal solution for problem 3
    git-svn-id: https://project-euler-in-java.googlecode.com/svn/trunk@5 2f9ebcba-fdc9-2f80-e423-1e4cc5a588e7

--- src/com/jwetherell/euler/Problem3.java
+++ src/com/jwetherell/euler/Problem3.java
@@ -9,15 +9,15 @@ package com.jwetherell.euler;
  */
 public class Problem3 {
     
-    private static final long NUMBER = 600851475143l;
+    private static long NUMBER = 600851475143l;
     
     public static final long solve() {
-        long result = Integer.MAX_VALUE;
+        long largest = 0;
         long sqrt = (long)Math.sqrt(NUMBER);
         for (long i=2; i<sqrt; i++) {
-            if (NUMBER%i==0 && isPrime(i)) result = i;
+            if (NUMBER%i==0 && isPrime(i)) largest = i;
         }
-        return result;
+        return largest;
     }
     
     private static final boolean isPrime(long value) {
@@ -30,6 +30,28 @@ public class Problem3 {
     public static final long ideal() {
         long largest = 0;
 
+        if (NUMBER%2==0) {
+            NUMBER = NUMBER/2;
+            largest = 2;
+            while (NUMBER%2==0) {
+                NUMBER = NUMBER/2;
+            }
+        } else {
+            largest = 1;
+        }
+        
+        int factor = 3;
+        while (NUMBER>1) {
+            if (NUMBER%factor==0) {
+                NUMBER = NUMBER/factor;
+                largest = factor;
+                while (NUMBER%factor==0) {
+                    NUMBER = NUMBER/factor;
+                }
+            }
+            factor += 2;
+        }
+        
         return largest;
     }
 }
